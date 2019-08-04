e28c7abe38cfe8be6cec063fdef7cf80ccc646c9
Oliver <ocben1@gmail.com>
2019-08-04 23:04:00 +1000

    added comments

--- VectorFind/Vector.cs
+++ VectorFind/Vector.cs
@@ -147,69 +147,75 @@ namespace Vector
 
         public T Max()
         {
-            Comparer<T> comparer = Comparer<T>.Default;
+            
+            Comparer<T> comparer = Comparer<T>.Default; //comparer to be used is the one specified in the tester file
 
-            if (data.Length == 0) return default(T);
+            if (data.Length == 0) return default(T); //if the sequence is empty, return the default value for type T.
 
-            T Max = data[0];
-            for (int i = 0; i < Count; i++)
+            T Max = data[0]; //Max is first element at 0th index of the sequence
+            for (int i = 0; i < Count; i++) //loop through elements in the sequence
             {
-                if (comparer.Compare(data[i], Max) == 1)
+                if (comparer.Compare(data[i], Max) == 1) //if the current element is larger than i...
                 {
-                    Max = data[i];
+                    Max = data[i]; //the current element is the new max
                 }
             }
-            return Max;
+            return Max; //return the maximum value in the sequence
         }
 
         public T Min()
         {
-            Comparer<T> comparer = Comparer<T>.Default;
+            Comparer<T> comparer = Comparer<T>.Default; //comparer to be used is the one specified in the tester file
 
-            if (data.Length == 0) return default(T);
+            if (data.Length == 0) return default(T); //if the sequence is empty, return the default value for type T.
 
-            T Min = data[0];
-            for (int i = 0; i < Count; i++)
+            T Min = data[0]; //Min is first element at 0th index of the sequence
+            for (int i = 0; i < Count; i++)  //loop through elements in the sequence
             {
-                if (comparer.Compare(data[i], Min) < 1)
+                if (comparer.Compare(data[i], Min) < 1) //if the currenr element is smaller than i...
                 {
-                    Min = data[i];
+                    Min = data[i]; //the current element is new minimum
                 }
             }
-            return Min;
+            return Min; //return the minimum value in the sequence
         }
 
         public Vector<T> FindAll(Func<T, bool> searchCriteria)
         {
+            //throw an error if no search criteria is specified 
             if (searchCriteria == null) throw new ArgumentNullException();
-
+            //new vector to contain all elements that match the search criteria
             Vector<T> FoundData = new Vector<T>(DEFAULT_CAPACITY);
             bool MatchesCriteria;
-            for (int i = 0; i < Count; i++)
+            
+            for (int i = 0; i < Count; i++) //access each element in the sequence
             {
-                MatchesCriteria = searchCriteria(data[i]);
-                if (MatchesCriteria == true)
+                MatchesCriteria = searchCriteria(data[i]); //does the current element match the search criteria?
+                if (MatchesCriteria == true) //if the current element matches the search criteria...
                 {
-                    FoundData.Add(data[i]);
+                    FoundData.Add(data[i]); //add it to the new sequence
                 }
             }
-            return FoundData;
+            return FoundData; //return the sequence of matching elements
         }
-
+        //this method returns the number of elements removed from Vector<T>
         public int RemoveAll(Func<T, bool> filterCriteria)
-        {
+        {   
+            //throw an error if no criteria for filtering is specified 
             if (filterCriteria == null) throw new ArgumentNullException();
+            
+            int RemovedData = 0; //initialise int 'RemovedData'. At this point no elements have been removed.
 
-            int RemovedData = 0;
-            for (int i = Count - 1; i >= 0; i--)
-            {
-                if (filterCriteria(data[i]))
+            //loop BACKWARDS through the sequence of elements, as each element after the element to be
+            //removed then decreases by 1 after removal.
+            for (int i = Count - 1; i >= 0; i--)             {
+                if (filterCriteria(data[i])) //if the current element meets the filter criteria
                 {
-                    RemovedData++;
-                    RemoveAt(i);
+                    RemovedData++; //increase the number of elements by 1
+                    RemoveAt(i); //remove the current element
                 }
             }
-            return RemovedData;
+            return RemovedData; //return the number of elements removed from the vector.
         }
     }
 }
